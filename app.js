085c5ac95a345e8d7d47c1baaf653b35cfcc1fb0
Amber Murray <amber.applebee@gmail.com>
2017-04-23 13:44:12 -0700

    preparing for heroku deployment

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
-app.use('/trucks', trucks)
-app.use('/bars', bars)
+// app.use('/trucks', trucks)
+// app.use('/bars', bars)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
