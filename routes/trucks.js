cbb466ab3835b23e47ae199fec3ba60ded1e94b9
Kay Krewson <kay.k@comcast.net>
2017-04-23 14:18:50 -0700

    added a comment to trucks.js

--- routes/trucks.js
+++ routes/trucks.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log("in the GET all trucks function");
   res.send('respond with a resource');
 });
 
