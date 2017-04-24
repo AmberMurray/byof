3d2cbda8384006a2798ac25b986f58de68c589c2
Kay Krewson <kay.k@comcast.net>
2017-04-23 19:10:11 -0700

    added get one truck route (trucks.js and show_truck.hbs

--- routes/trucks.js
+++ routes/trucks.js
@@ -5,9 +5,24 @@ var knex = require('../db/connection.js')
 // ===== GET ALL TRUCKS =====
   router.get('/', function(req, res, next) {
   console.log("In the GET all trucks function")
-  knex('trucks').select('*').then(trucks => {
-    console.log('trucks is ', trucks);
+  knex('trucks').select('*')
+  .then(trucks => {
+    // console.log('trucks is ', trucks)
     res.render('trucks', { trucks })
+  })
+})
+
+// ===== GET ONE TRUCK =====
+  router.get('/:id', function(req, res, next) {
+  console.log("In the GET one truck function")
+  var id = req.params.id
+  knex('trucks')
+  .select('*')
+  .where('id', id)
+  .first()
+  .then(truck => {
+    console.log('truck is ', truck)
+    res.render('show_truck', { truck })
   })
 })
 
