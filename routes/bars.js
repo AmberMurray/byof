34507c036e725e0d3d088f4f1873ca2148d3690c
Kay Krewson <kay.k@comcast.net>
2017-04-23 20:20:58 -0700

    re-fixed the app.use for bars in app.js, redid bars get all route

---
+++ routes/bars.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/connection.js')
+
+// ===== GET ALL BARS =====
+  router.get('/', function(req, res, next) {
+  console.log("In the GET all bars function")
+  knex('bars').select('*')
+  .then(bars => {
+    console.log('bars is ', bars)
+    res.render('bars', { bars })
+  })
+})
+
+// ===== GET ONE BAR =====
+  router.get('/:id', function(req, res, next) {
+  console.log("In the GET one bar function")
+  var id = req.params.id
+  knex('bars')
+  .select('*')
+  .where('id', id)
+  .first()
+  .then(bar => {
+    console.log('bar is ', bar)
+    res.render('show_bar', { bar })
+  })
+})
+
+module.exports = router;
