e48f9669bf4bb97efce03f8e7732a99cedea1da7
Kay Krewson <kay.k@comcast.net>
2017-04-24 08:24:37 -0700

    typo in previous git commands, redo-ing

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var knex = require('../db/connection.js')
 
 // ===== AUTHORIZATION =====
-const authorize = function(req, res, next) {
+let authorize = function(req, res, next) {
   if (!req.session.userId) {
     return next({
       status: 401,
@@ -15,7 +15,7 @@ const authorize = function(req, res, next) {
 
 // ===== GET FAVORITE TRUCKS =====
 router.get('/favorites', authorize, (req, res, next) => {
-  const { userId } = req.session
+  let { userId } = req.session
 
   knex('favorites')
     .innerJoin('trucks', 'trucks.id', 'favorites.truck_id')
@@ -29,11 +29,33 @@ router.get('/favorites', authorize, (req, res, next) => {
     })
 })
 
+// ===== CHECK TO SEE IF TRUCK IS A FAVORITE =====
+router.get('/favorites/check', authorize, (req, res, next) => {
+  let { userId } = req.session
+  let query = req.query
+  let queryId = req.query.id
+
+  knex('favorites')
+    .innerJoin('trucks', 'trucks.id', 'favorites.truck_id')
+    .where(query)
+    .orderBy('trucks.name', 'food')
+    .then((favorite) => {
+      if (favorite == 0) {
+        res.status(200).send(false)
+      }
+      else {
+        res.status(200).send(true)
+      }
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
 // ====== ADD A TRUCK TO FAVORITES =====
 router.post('/favorites', authorize, (req, res, next) => {
-  const { userId } = req.session
-  const newFavorite = req.body
-  const newFavId = req.body.truck_id
+  let { userId } = req.session
+  let newFavorite = req.body
+  let newFavId = req.body.truck_id
 
 knex('favorites')
   .insert({truck_id: newFavId, user_id: userId}, "*")
@@ -46,3 +68,26 @@ knex('favorites')
     next(err)
   })
 })
+
+// ====== DELETE A TRUCK FROM FAVORITES LIST =====
+router.delete('/favorites', authorize, (req, res, next) => {
+  let { userId } = req.session
+  let toBeRemoved = req.body
+  let id = req.body.truck_id
+
+  if (!userId) {
+    return next({
+      status: 401,
+      message: 'Unauthorized'
+    })
+  }
+    knex('favorites')
+    .where('id', id)
+    .delete()
+    .returning(['truck_id', 'user_id'])
+    .then((result) => {
+      res.json(result[0])
+    })
+})
+
+module.exports = router
