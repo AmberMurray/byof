097e1df474086958ffd0b6eb23150e79f1540120
Kay Krewson <kay.k@comcast.net>
2017-04-26 16:32:27 -0700

    successfully adding a user now

--- routes/trucks.js
+++ routes/trucks.js
@@ -7,32 +7,16 @@ var rp = require('request-promise')
 
 // ===== GET ALL TRUCKS =====
 router.get('/', function(req, res, next) {
-  console.log("In the GET all trucks function")
+  // console.log("In the GET all trucks function")
   knex('trucks').select('*')
   .then(trucks => {
     res.render('trucks', { trucks })
   })
 })
 
-// // ===== GET ALL BARS NEAR A PARTICULAR TRUCK =====
-//   router.get('/:id/schedules', function(req, res, next) {
-//   console.log("In the GET schedule function")
-//   console.log("req.params.id is ", req.params.id)
-//   id = req.params.id
-//
-//   knex('trucks').select('*')
-//   .innerJoin('schedules', 'trucks.id', 'schedules.truck_id')
-//   .innerJoin('bars', 'schedules.bar_id', 'bars.id')
-//    .where('schedules.truck_id', id)
-//    .then(nearby => {
-//      console.log('nearby is ', nearby)
-//      res.render('show_truck', nearby )
-//    })
-// })
-
 // ===== GET ONE TRUCK =====
 router.get('/:id', function(req, res, next) {
-  console.log("In the GET one truck function")
+  // console.log("In the GET one truck function")
   var truckId = req.params.id
 
   let truckComments = knex('comments').where('truck_id', truckId)
@@ -52,7 +36,7 @@ router.get('/:id', function(req, res, next) {
   .select('business_id')
   .returning('*')
   .then((businessIdObj) => {
-    console.log('bus id is now ', businessIdObj[0].business_id)
+    // console.log('bus id is now ', businessIdObj[0].business_id)
     const businessId = businessIdObj[0].business_id
     return businessId
   })
@@ -61,7 +45,7 @@ router.get('/:id', function(req, res, next) {
     let queryParam = `Business_ID=${businessId}`
     let fullQuery = queryRoot + queryParam
 
-    console.log('full query is ', fullQuery);
+    // console.log('full query is ', fullQuery);
 
     // make the query
     const sodaQuery = rp({
@@ -72,7 +56,7 @@ router.get('/:id', function(req, res, next) {
 
     Promise.all([truckComments, truckSched, truckInfo, sodaQuery])
     .then((results) => {
-      console.log('results', results)
+      // console.log('results', results)
 
       let monsterTruck = {
         commentDeets: results[0],
@@ -81,7 +65,7 @@ router.get('/:id', function(req, res, next) {
         inspectionResult: results[3][0].inspection_result,
         inspectionDate: results[3][0].inspection_date
       }
-      console.log('monster truck ', monsterTruck)
+      // console.log('monster truck ', monsterTruck)
       res.render('show_truck', {monsterTruck})
     })
 
