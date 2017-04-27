92e98ef7cf00007b43238d1648abab734273e171
Amber Murray <amber.applebee@gmail.com>
2017-04-27 14:28:15 -0700

    error checking for message adding with no login

--- routes/comments.js
+++ routes/comments.js
@@ -5,29 +5,22 @@ var knex = require('../db/connection.js')
 // ===== AUTHORIZATION =====
 let authorize = function(req, res, next) {
   if (!req.session.userId) {
-    return next({
-      status: 401,
-      message: 'Unauthorized'
-    })
+    res.redirect('/login')
   }
   next()
 }
 
 // ===== ADD A COMMENT ABOUT A TRUCK =====
-router.post('/', (req, res, next) => {
-  console.log('in the Add a comment function, req.body is ', req.body)
-  console.log('req.params is ', req.params)
-
-  // let { userId } = req.session
+router.post('/', authorize, (req, res, next) => {
+  let { userId } = req.session
   let comment = req.body
-  let userId = 2
+
   const addComment = { user_id: userId, truck_id: comment.truck_id, review: comment.review }
 
   knex('comments')
     .insert(addComment, '*')
     .then((rows) => {
       let comment = rows[0]
-      console.log('adding a comment ', comment)
       res.redirect(`/trucks/${comment.truck_id}` )
     })
     .catch((err) => {
